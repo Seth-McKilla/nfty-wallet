fbf60c9f6cee0efd0da1252fb4da24af82ec2267
Seth McCullough <seth@endevrs.dev>
2022-02-10 13:35:41 -0500

    Finalize card component

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,23 +1,45 @@
-import { Image, Box } from "@chakra-ui/react";
+import { Image, Box, Link } from "@chakra-ui/react";
 
-export default function Card({ name, imageUrl, price }) {
+export default function Card({ name, collectionName, permalink, imageUrl }) {
   return (
-    <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
-      <Image src={imageUrl} alt={name} />
+    <Link href={permalink} isExternal style={{ textDecoration: "none" }}>
+      <Box
+        w="xs"
+        borderWidth="1px"
+        borderRadius="lg"
+        overflow="hidden"
+        cursor="pointer"
+        _hover={{
+          borderColor: "purple.500",
+          color: "purple.500",
+          stroke: "blue",
+          boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.25)",
+        }}
+      >
+        <Image
+          w="xs"
+          h="300px"
+          src={imageUrl}
+          fallbackSrc="/images/placeholder.jpg"
+          alt={name}
+          fit="cover"
+        />
 
-      <Box p="6">
-        <Box
-          mt="1"
-          fontWeight="semibold"
-          as="h4"
-          lineHeight="tight"
-          isTruncated
-        >
-          {name}
+        <Box p="6">
+          <Box
+            mt="1"
+            fontWeight="semibold"
+            fontSize="xl"
+            as="h4"
+            lineHeight="tight"
+            isTruncated
+          >
+            {name}
+          </Box>
+
+          <Box>{collectionName}</Box>
         </Box>
-
-        <Box>{price}</Box>
       </Box>
-    </Box>
+    </Link>
   );
 }
--- src/views/CollectionView/index.jsx
+++ src/views/CollectionView/index.jsx
@@ -1,25 +1,52 @@
-import { Center, Flex, Text, Spinner } from "@chakra-ui/react";
-import { UDLogin, Card } from "../../components";
+import { Center, Flex, Text, useDisclosure } from "@chakra-ui/react";
+import { UDLogin, Loading, Card, Modal } from "../../components";
 import useOpensea from "../../hooks/useOpensea";
 
 export default function LandingView({ onLogin, userAddress }) {
   userAddress = "0x9C0177e8dDA1D562149F33B6Af8014228ca11E44";
   const { loading, error, nfts } = useOpensea(userAddress);
+  console.log(nfts);
+  const { isOpen, onClose } = useDisclosure();
+
+  const renderContent = () => {
+    if (loading) {
+      return <Loading />;
+    } else {
+      return nfts.map(({ name, collection, permalink, image_url }) => (
+        <Card
+          key={permalink}
+          name={name}
+          collectionName={collection.name}
+          permalink={permalink}
+          imageUrl={image_url}
+        />
+      ));
+    }
+  };
 
   return (
-    <Center h="calc(100vh - 72px)" ml={6} mr={6}>
-      <Flex direction="column" align="center">
-        {true ? (
-          <Card />
-        ) : (
-          <>
-            <Text fontSize="4xl" align="center" mb={4}>
-              Please login to view your NFT collection
-            </Text>
-            <UDLogin onLogin={onLogin} />
-          </>
-        )}
-      </Flex>
-    </Center>
+    <>
+      <Modal
+        title="Oops! Something went wrong"
+        body={error}
+        isOpen={isOpen}
+        onClose={onClose}
+      />
+
+      <Center h="calc(100vh - 72px)" ml={6} mr={6}>
+        <Flex direction="column" align="center">
+          {true ? (
+            renderContent()
+          ) : (
+            <>
+              <Text fontSize="4xl" align="center" mb={4}>
+                Please login to view your NFT collection
+              </Text>
+              <UDLogin onLogin={onLogin} />
+            </>
+          )}
+        </Flex>
+      </Center>
+    </>
   );
 }
