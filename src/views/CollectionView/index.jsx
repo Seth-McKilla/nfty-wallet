04e0cbc3ee2d6c44fc4a9762b53249f10e53abe2
Seth McCullough <seth@endevrs.dev>
2022-02-10 14:31:03 -0500

    Add search bar

--- src/views/CollectionView/index.jsx
+++ src/views/CollectionView/index.jsx
@@ -1,18 +1,39 @@
+import { useState, useEffect } from "react";
 import {
   Grid,
   GridItem,
   Center,
   Flex,
   Text,
+  Input,
   useDisclosure,
 } from "@chakra-ui/react";
 import { UDLogin, Loading, Card, Modal } from "../../components";
 import useOpensea from "../../hooks/useOpensea";
 
 export default function LandingView({ onLogin, userAddress }) {
-  userAddress = "0x9C0177e8dDA1D562149F33B6Af8014228ca11E44";
   const { loading, error, nfts } = useOpensea(userAddress);
   const { isOpen, onClose } = useDisclosure();
+  const [search, setSearch] = useState("");
+  const [filteredNfts, setFilteredNfts] = useState([]);
+
+  const handleChange = (e) => {
+    const search = e.target.value;
+
+    setSearch(search);
+    setFilteredNfts(
+      nfts.filter(({ name, collection }) => {
+        const nameAndCollection = `${name} ${collection.name}`;
+        return nameAndCollection.toLowerCase().includes(search.toLowerCase());
+      })
+    );
+  };
+
+  useEffect(() => {
+    if (nfts) {
+      setFilteredNfts(nfts);
+    }
+  }, [nfts]);
 
   const renderContent = () => {
     if (loading) {
@@ -23,28 +44,37 @@ export default function LandingView({ onLogin, userAddress }) {
       );
     } else {
       return (
-        <Grid
-          templateColumns={{
-            base: "1fr",
-            lg: "repeat(2, 1fr)",
-            xl: "repeat(3, 1fr)",
-            "2xl": "repeat(4, 1fr)",
-          }}
-          gap={6}
-          mt="125"
-          mb="50"
-        >
-          {nfts.map(({ name, collection, permalink, image_url }) => (
-            <GridItem key={permalink}>
-              <Card
-                name={name}
-                collectionName={collection.name}
-                permalink={permalink}
-                imageUrl={image_url}
-              />
-            </GridItem>
-          ))}
-        </Grid>
+        <>
+          <Input
+            placeholder="Search NFTs..."
+            w="sm"
+            mt="100"
+            onChange={handleChange}
+            value={search}
+          />
+          <Grid
+            templateColumns={{
+              base: "1fr",
+              lg: "repeat(2, 1fr)",
+              xl: "repeat(3, 1fr)",
+              "2xl": "repeat(4, 1fr)",
+            }}
+            gap={6}
+            mt="30"
+            mb="50"
+          >
+            {filteredNfts.map(({ name, collection, permalink, image_url }) => (
+              <GridItem key={permalink}>
+                <Card
+                  name={name}
+                  collectionName={collection.name}
+                  permalink={permalink}
+                  imageUrl={image_url}
+                />
+              </GridItem>
+            ))}
+          </Grid>
+        </>
       );
     }
   };
@@ -59,7 +89,7 @@ export default function LandingView({ onLogin, userAddress }) {
       />
 
       <Flex direction="column" align="center">
-        {true ? (
+        {userAddress ? (
           renderContent()
         ) : (
           <>
